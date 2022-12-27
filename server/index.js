d5a5a2296c30742433bd4ef382a8c8000bbe0762
mohammad mohammadi <dev.mohammadii@gmail.com>
2022-12-27 15:26:30 +0330

    add base uplaod file :multer

--- server/index.js
+++ server/index.js
@@ -22,3 +22,13 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 app.use("/assets",express.static(path.join(__dirname,"public/assets")));
 
+// File storage
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"public/assets");
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname);
+    },
+});
+const upload = multer({storage});
