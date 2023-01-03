2601c14a86a02861c432eb20f1d50c967e1c1e53
mohammad mohammadi <dev.mohammadii@gmail.com>
2023-01-03 09:29:47 +0330

    add userFriends

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -10,3 +10,20 @@ export const getUer =async (req,res)=>{
         res.status(404).json({message:err.message});
     }
 }
+export const getUserFriends = async(req,res)=>{
+    try {
+        const {id}= req.params;
+        const user=await User.findById(id);
+        const friends = await Promise.all(
+            user.friends.map((id)=>User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({_id,fristName,lastName,occupation,locaion,picturePath})=> {
+                return{_id,fristName,lastName,occupation,locaion,picturePath}
+            }
+        );
+        res.status(200).json(formattedFriends);
+    } catch (err) {
+        res.status(404).json({message:err.message});
+    }
+}
