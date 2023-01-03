f24d1edeee9d00711c54af0bbfa1c2511732d2e0
mohammad mohammadi <dev.mohammadii@gmail.com>
2023-01-03 21:02:58 +0330

    add : like unlike post functionaliti

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -26,3 +26,42 @@ export const createPost = async(req,res)=>{
 }
 
 
+export const getFeedPosts = async(req,res)=>{
+    try {
+        const post = await Post.find();
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
+}
+export const getUserPosts = async(req,res)=>{
+    try {
+        const {userId} = req.params;
+        const post = await Post.find({userId});
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
+}
+export const likePost = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const {userId} = req.body;
+        const post = await Post.find(id);
+        const isLiked = post.likes.get(userId);
+        if(isLiked){
+            post.likes.delet(userId);
+        }else{
+            post.likes.set(userId,true)
+        }
+        
+        const updatedPost = await Post.findByIdAndUpdate(
+            id,
+            {likes:post.likes},
+            {new:true}
+        )
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(404).json({message:error.message})
+    }
+}
