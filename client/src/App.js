79ac0452ab087abac067c5336531a25b98096992
mohammad mohammadi <dev.mohammadii@gmail.com>
2023-01-07 22:56:38 +0330

    add flexBetween

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import {BrowserRouter,Navigate,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import ProfilePage from "./scenes/profilePage";
-import {useSelector} from "react-redux";
-import {CssBaseline,ThemeProvider} from "@mui/material"
-import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/system";
 import { themeSettings } from "./theme";
 
 function App() {
-  const mode = useSelector((state)=>state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div className="App">
+    <div className="app">
     <BrowserRouter>
+
     <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/home" element={<HomePage/>}/>
-        <Route path="/profile/:userId" element={<ProfilePage/>}/>
-      </Routes>
+
+    {/* <CssBaseline/> */}
+        <Routes>
+          <Route path="/" element={<LoginPage/>}/>
+          <Route path="/home" element={<HomePage/>}/>
+          <Route path="/profile/:userId" element={<ProfilePage/>}/>
+        </Routes>
      </ThemeProvider>
     </BrowserRouter>
     </div>
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,7 +1,7 @@
 import {createSlice} from "@reduxjs/toolkit"
 
 const initialState ={
-    mode:"Light",
+    mode:"light",
     user:null,
     token:null,
     posts:[]
