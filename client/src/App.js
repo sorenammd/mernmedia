c766ad09d39ff378d642d55108c99fa80e9f29fd
mohammad mohammadi <dev.mohammadii@gmail.com>
2023-01-09 09:42:34 +0330

    fix code , upgrate packegs ,add felxBetween comp

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,31 @@ import ProfilePage from "./scenes/profilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/system";
+import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-    <BrowserRouter>
-
-    <ThemeProvider theme={theme}>
-
-    {/* <CssBaseline/> */}
-        <Routes>
-          <Route path="/" element={<LoginPage/>}/>
-          <Route path="/home" element={<HomePage/>}/>
-          <Route path="/profile/:userId" element={<ProfilePage/>}/>
-        </Routes>
-     </ThemeProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={ <HomePage /> }
+            />
+            <Route
+              path="/profile/:userId"
+              element={ <ProfilePage /> }
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
